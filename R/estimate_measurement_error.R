342c9ae7674d78c4b55d3f38c673a4832abd13a2
Eva Marques <marquesel@gn040801.hpc.niehs.nih.gov>
2024-04-16 17:47:02 -0400

    Measurement error functions and vignette

--- R/estimate_measurement_error.R
+++ R/estimate_measurement_error.R
@@ -1,34 +1,52 @@
-find_closest_ref <- function(x, ref) {
-  stopifnot("x is not a sf" = is(x, "sf"),
+#' Find the closest reference site for each site in a cws object
+#' @param cws sf object with citizen weather stations
+#' @param ref sf object with reference weather stations
+#' @return sf object with the closest reference site for each cws
+#' with additional columns ref_id, ref_geometry and dist_to_ref
+#' @export
+find_closest_ref <- function(cws, ref) {
+  stopifnot("cws is not a sf" = is(cws, "sf"),
             "ref is not a sf" = is(ref, "sf"))
-  nearest <- sf::st_nearest_feature(x, ref)
-  return(ref[nearest, ])
+  # check column names
+  cols <- c("site_id", "geometry")
+  stopifnot("some columns missing in cws" = all(cols %in% colnames(cws)),
+            "some columns missing in ref" = all(cols %in% colnames(ref)))
+  cws_loc <- unique(cws[, cols])
+  ref_loc <- unique(ref[, cols])
+  nearest <- sf::st_nearest_feature(cws_loc, ref_loc)
+  ref_nearest <- ref_loc[nearest, ]
+  cws_loc$ref_id <- ref_nearest$site_id
+  cws_loc$ref_geometry <- ref_nearest$geometry
+  cws_loc$dist_to_ref <- sf::st_distance(cws_loc$geometry,
+                                         cws_loc$ref_geometry,
+                                         by_element = TRUE) |>
+    as.numeric()
+  cws_loc$ref_geometry <- NULL
+  r <- merge(cws,
+        as.data.frame(cws_loc[, c("site_id", "ref_id", "dist_to_ref")],
+        by = "site_id"))
+  return(r)
 }
 
-#' Add temperature from a reference to a hourly_temp object
-#' @param x hourly_temp object
-#' @param ref hourly_temp object with the closest reference temperature
+#' Add temperature from a reference to citizen weather stations observations
+#' @param cws sf object with citizen weather stations
+#' @param ref sf object with reference weather stations
 #' @return hourly_temp object with the reference temperature in columns temp_r
-add_temp_reference <- function(x, ref) {
-  if (is(x, "sftime")) {
-    x <- x |> sftime::st_as_sftime(coords = c("lon", "lat"),
-                                   time_column_name = "time",
-                                   crs = 4326,
-                                   remove = FALSE)
-  }
-  if (class(ref) != "sftime") {
-    ref <- ref |> sftime::st_as_sftime(coords = c("lon", "lat"),
-                                      time_column_name = "time",
-                                      crs = 4326,
-                                      remove = FALSE)
-  }
-
+calc_temp_error <- function(cws, ref) {
+  # check column names
+  cols <- c("site_id", "temp", "geometry", "time")
+  stopifnot("some columns missing in cws" = all(cols %in% colnames(cws)),
+            "some columns missing in ref" = all(cols %in% colnames(ref)))
+  cws_r <- find_closest_ref(cws, ref)
+  ref_reformat <- ref[, c("site_id",
+                          "temp",
+                          "time")] |>
+    dplyr::rename(temp_ref = "temp") |>
+    dplyr::rename(ref_id = "site_id") |>
+    as.data.frame()
+  ref_reformat$geometry <- NULL
+  r <- merge(cws_r, ref_reformat, by = c("time", "ref_id"), all.x = T)
+  r$temp_err <- r$temp - r$temp_ref
+  return(r)
 }
 
-
-#' Estimate measurement errors for a hourly_temp object
-#'
-estimate_temp_error <- function(x, ref) {
-  x <- add_temp_reference(x, ref)
-
-}

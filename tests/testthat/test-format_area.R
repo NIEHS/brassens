7f71ba3ed863a2ec14b22cb85f800ded3bf3f45e
Eva Marques <marquesel@gn040801.hpc.niehs.nih.gov>
2024-06-06 13:25:15 -0400

    test format_area

--- tests/testthat/test-format_area.R
+++ tests/testthat/test-format_area.R
@@ -1,4 +1,22 @@
 testthat::test_that("format_area works well", {
-
+  expect_error(format_area("a"))
+  triangle <- rbind(
+    c(-79.19, 35.6),
+    c(-79.19, 36.11),
+    c(-78.39, 36.11),
+    c(-78.39, 35.6)
+  ) |>
+    terra::vect("polygons", crs = "epsg:4326")
+  expect_no_error(format_area(triangle))
+  expect_no_error(inherits(format_area(triangle), c("sf", "sfc")))
+  expect_no_error(format_area(format_area(triangle)))
+  x <- terra::rast(nrows = 10,
+            ncols = 10,
+            xmin = -79.19,
+            xmax = -78.39,
+            ymin = 35.6,
+            ymax = 36.11)
+  terra::values(x) <- 1:100
+  expect_no_error(format_area(x))
 })
 

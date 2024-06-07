9818f236897d2b66e3d63df562944a2c55db36b5
Eva Marques <marquesel@gn040801.hpc.niehs.nih.gov>
2024-06-07 11:10:29 -0400

    test generate_site_id

--- R/generate_unique_site_id.R
+++ R/generate_unique_site_id.R
@@ -1,8 +1,8 @@
 #` @title Generate unique sensor ID
 #` @description This function generates a unique sensor "id" column based on
 #` the latitude and longitude of the sensor.
-#` @param x A data frame with columns "lat" and "lon".
-#` @return A data frame with an additional column "id".
+#` @param x A data.frame, data.table, sf or sftime with columns "lat" and "lon"
+#` @return A data frame with an additional column "id"
 #` @author Eva Marques
 generate_site_id <- function(x) {
   stopifnot("lat and lon columns are missing" =

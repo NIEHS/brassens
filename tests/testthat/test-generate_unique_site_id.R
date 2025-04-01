cc2d733b63dcb0adc5343331e8b8ce1959261f6f
Eva Marques <marquesel@cn040603.hpc.niehs.nih.gov>
2025-04-01 14:24:24 -0400

    remove sftime test

--- tests/testthat/test-generate_unique_site_id.R
+++ tests/testthat/test-generate_unique_site_id.R
@@ -5,15 +5,6 @@ testthat::test_that("generate_unique_site_id works well", {
   x_sf <- x |>
     sf::st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE)
   expect_no_error(generate_site_id(x_sf))
-  # convert to sftime:
-  x_sftime <- x |>
-    sftime::st_as_sftime(
-      coords = c("lon", "lat"),
-      time_column_name = "obsTimeUtc",
-      crs = 4326,
-      remove = FALSE
-    )
-  expect_no_error(generate_site_id(x_sftime))
   # lat and lon missing:
   expect_error(generate_site_id(x[, c("stationID", "obsTimeUtc")]))
   # some tests on site_id:
